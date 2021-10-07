51b77a6b7558f01a3a080b2a886db2329918199c
carlosWirama <carloswirama@gmail.com>
2021-10-08 00:45:26 +0800

    format acceptance tests

--- src/acceptanceTests/acceptance.test.ts
+++ src/acceptanceTests/acceptance.test.ts
@@ -1,15 +1,31 @@
-Given I navigate to the user interface and create a PAL SD Video Reel
-When I try and add NTSC SD video clip
-Then user interface should prevent me from doing this action
+describe('Acceptance tests', () => {
 
-Given I navigate to the user interface and create a PAL SD Video Reel
-When I try and add PAL HD video clip
-Then user interface should prevent me from doing this action
+  describe('Given I navigate to the user interface and create a PAL SD Video Reel', () => {
+    describe('When I try and add NTSC SD video clip', () => {
+      test('user interface should prevent me from doing this action', () => {
+        //
+      });
+    });
 
-Given I navigate to the user interface and create a PAL SD Video Reel
-When I add all the PAL SD video clips
-Then the total duration displayed is 00:02:11:01
+    describe('When I try and add PAL HD video clip', () => {
+      test('user interface should prevent me from doing this action', () => {
+        // 
+      });
+    });
 
-Given I navigate to the user interface and create a NTSC SD Video Reel
-When I add all the NTSC SD video clips
-Then the total duration displayed is 00:00:54:08
+    describe('When I add all the PAL SD video clips', () => {
+      test('the total duration displayed is 00:02:11:01', () => {
+        // 
+      });
+    });
+  });
+
+  describe('Given I navigate to the user interface and create a NTSC SD Video Reel', () => {
+    describe('When I add all the NTSC SD video clips', () => {
+      test('the total duration displayed is 00:00:54:08', () => {
+        // 
+      });
+    });
+  });
+
+});
