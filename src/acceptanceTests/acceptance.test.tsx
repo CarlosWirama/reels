e2e13fd45a1ff3fa810eef75ab6350c99c5d79a0
carlosWirama <carloswirama@gmail.com>
2021-10-08 03:27:54 +0800

    improved test for the first 2 cases

--- src/acceptanceTests/acceptance.test.tsx
+++ src/acceptanceTests/acceptance.test.tsx
@@ -2,25 +2,44 @@ import { render, screen, fireEvent, act } from '@testing-library/react';
 import App from '../App';
 
 describe('Acceptance tests', () => {
-  const a = screen.getAllByTestId
+  test('initial state', () => {
+    render(<App />);
+    const timelineTiles = screen.queryAllByTestId('timeline-tile');
+    expect(timelineTiles).toHaveLength(0);
+  });
+
   describe('Given I navigate to the user interface and create a PAL SD Video Reel', () => {
+    const createPalSdVideoReel = () => {
+      render(<App />);
+      const [
+        palSdClip1,
+        ntscSdClip,
+        palSdClip2,
+        palSdClip3,
+        ___,
+        ____,
+        palHdClip,
+      ] = screen.getAllByTestId('gallery-tile');
+  
+      act(() => {
+        fireEvent.click(palSdClip1);
+      });
+
+      return { ntscSdClip, palSdClip2, palSdClip3, palHdClip }
+    };
+
+    test('create a PAL SD Video Reel', () => {
+      createPalSdVideoReel();
+
+      const timelineTiles2 = screen.getAllByTestId('timeline-tile');
+      expect(timelineTiles2).toHaveLength(1);
+  
+    });
     describe('When I try and add NTSC SD video clip', () => {
       test('user interface should prevent me from doing this action', () => {
-        render(<App />);
-        const [palSdClip1, ntscSdClip1] = screen.getAllByTestId('gallery-tile');
-
-        const timelineTiles1 = screen.queryAllByTestId('timeline-tile');
-        expect(timelineTiles1).toHaveLength(0);
-
+        const { ntscSdClip } = createPalSdVideoReel();
         act(() => {
-          fireEvent.click(palSdClip1);
-        });
-        
-        const timelineTiles2 = screen.getAllByTestId('timeline-tile');
-        expect(timelineTiles2).toHaveLength(1);
-
-        act(() => {
-          fireEvent.click(ntscSdClip1);
+          fireEvent.click(ntscSdClip);
         });
         const timelineTiles3 = screen.getAllByTestId('timeline-tile');
         expect(timelineTiles3).toHaveLength(1);
@@ -29,13 +48,26 @@ describe('Acceptance tests', () => {
 
     describe('When I try and add PAL HD video clip', () => {
       test('user interface should prevent me from doing this action', () => {
-        // 
+        const { palHdClip } = createPalSdVideoReel();
+        act(() => {
+          fireEvent.click(palHdClip);
+        });
+        const timelineTiles3 = screen.getAllByTestId('timeline-tile');
+        expect(timelineTiles3).toHaveLength(1);
       });
     });
 
     describe('When I add all the PAL SD video clips', () => {
       test('the total duration displayed is 00:02:11:01', () => {
-        // 
+        const { palSdClip2, palSdClip3 } = createPalSdVideoReel();
+        act(() => {
+          fireEvent.click(palSdClip2);
+        });
+        act(() => {
+          fireEvent.click(palSdClip3);
+        });
+        const timelineTiles3 = screen.getAllByTestId('timeline-tile');
+        expect(timelineTiles3).toHaveLength(3);
       });
     });
   });
